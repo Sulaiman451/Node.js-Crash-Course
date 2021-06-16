c7bb90b739b69c3c44dadc4c0f2c17ac7e8b9b0d
Sulaiman Rahman <rsulaiman451@gmail.com>
2021-06-16 16:11:25 +0100

    Write code for GET, POST & DELETE requests

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
 const Blog = require("./models/blog");
+const { response } = require("express");
+const { render } = require("ejs");
 
 // express app
 const app = express();
@@ -19,6 +21,7 @@ app.set("view engine", "ejs");
 
 // middleware & static files
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
 
 // mongoose and mongo sandbox routes
@@ -76,8 +79,45 @@ app.get("/blogs", (req, res) => {
     });
 });
 
+app.post("/blogs", (req, res) => {
+  const blog = new Blog(req.body);
+
+  blog
+    .save()
+    .then((result) => {
+      res.redirect("/blogs");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.get("/blogs/:id", (req, res) => {
+  const id = req.params.id;
+  Blog.findById(id)
+    .then((result) => {
+      res.render("details", { blog: result, title: "Blog Details" });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
 app.get("/blogs/create", (req, res) => {
   res.render("create", { title: "Create a new blog" });
+});
+
+app.delete("/blogs/:id", (req, res) => {
+  const id = req.params.id;
+
+  Blog.findByIdAndDelete(id)
+    .then((result) => {
+      res.json({ redirect: "/blogs" });
+    })
+    .then((data) => (window.location.href = data.redirect))
+    .catch((err) => {
+      console.log(err);
+    });
 });
 
 // 404 page
